54a52dd6727c47d883a5767e51149df388d10f88
fitmariusz <fitmariusz@gmail.com>
2024-02-11 17:17:29 +0100

    defin const

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -25,7 +25,7 @@ form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   for (let i = 0; i < Number(form.elements.amount.value); i += 1){
     const delayTime = Number(form.elements.delay.value) + Number(form.elements.step.value) * i;
-    timerId = setTimeout((position, delay) => {
+    const timerId = setTimeout((position, delay) => {
       myPromise(position, delay);
     }, delayTime, i + 1, delayTime);
   };
